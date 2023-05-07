c858e16886d793848af0e7c919ffb20518c3a33b
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2023-05-07 16:03:47 -0500

    Avanzando bastanteasa

---
+++ app/src/main/java/com/jhon/gen_dorado_oficial/Objetos/Historialmedicamento.java
@@ -0,0 +1,45 @@
+package com.jhon.gen_dorado_oficial.Objetos;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Historialmedicamento {
+    int numdosis;
+    String horatomada;
+    String calculotomado;
+
+    public Historialmedicamento() {
+    }
+
+    public Historialmedicamento(int numdosis, String horatomada, String calculotomado) {
+        this.numdosis = numdosis;
+        this.horatomada = horatomada;
+        this.calculotomado = calculotomado;
+    }
+
+    public int getNumdosis() {
+        String numtext = String.valueOf(this.numdosis);
+        return numdosis;
+    }
+
+    public void setNumdosis(int numdosis) {
+        this.numdosis = numdosis;
+    }
+
+    public String getHoratomada() {
+        return horatomada;
+    }
+
+    public void setHoratomada(String horatomada) {
+        this.horatomada = horatomada;
+    }
+
+    public String getCalculotomado() {
+        //Calculo tomado
+        return calculotomado;
+    }
+
+    public void setCalculotomado(String calculotomado) {
+        this.calculotomado = calculotomado;
+    }
+}
